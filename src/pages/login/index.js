39ed90e12de2b84cc294da58be18061942415e87
Flavio <flaviomatheus28013@gmail.com>
2024-11-04 20:17:15 -0300

    stylescomponets

---
+++ src/pages/login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container } from "../../styles/GlobalStyles";
+import { Title, Paragrafo} from "./styled";
+
+
+export default function login(){
+    return (
+        <Container>
+            <Title>Login
+                <small> Oie</small>
+            </Title>
+                <Paragrafo>lorem lalsksnde ksdn ej cej</Paragrafo>
+                <button type="button">Enviar</button>
+        </Container>
+    )
+}
+
